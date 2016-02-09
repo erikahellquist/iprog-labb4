f53780d0f0409b1cf437285761e5e3ccf7c343ed
Erika Hellquist <erika.hellquist@live.com>
2016-02-09 15:43:48 +0100

    create new controllers

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ $(function() {
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
+
 	var startView = new StartView($("#startView"), model);
+	var selectDishView = new SelectDishView($("#selectDishView"), model);
+	var dishOverviewView = new DishOverviewView($("#dishOverviewView"), model);
+	var dinnerOverviewView = new DinnerOverviewView($("#dinnerOverviewView"), model);
+	var dinnerPreparationView = new DinnerPreparationView($("#dinnerPreparationView"), model);
+
 
 });
