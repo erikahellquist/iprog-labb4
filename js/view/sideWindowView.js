9ce741f149aa0bc2577c66e5d35f20ff76a41e44
Erika Hellquist <erika.hellquist@live.com>
2016-02-16 22:43:31 +0100

    more beauty changes

--- js/view/sideWindowView.js
+++ js/view/sideWindowView.js
@@ -10,6 +10,12 @@ var SideWindowView = function (container, model) {
 	this.starter = container.find("#starter");
 	this.main = container.find("#main");
 	this.dessert = container.find("#dessert");
+
+	this.starterPrice = container.find("#starterPrice");
+	this.mainPrice = container.find("#mainPrice");
+	this.dessertPrice = container.find("#dessertPrice");
+
+	this.totalPrice = container.find("#totalPrice");
 	
 	this.numberOfGuests.html(model.getNumberOfGuests());
 
@@ -17,12 +23,17 @@ var SideWindowView = function (container, model) {
 
 	var start = model.getDish(dishList[0]);
 	this.starter.html(start.name);
+	this.starterPrice.html(model.getDishGuestPrice(start.id));
 
 	var main = model.getDish(dishList[1]);
 	this.main.html(main.name);
+	this.mainPrice.html(model.getDishGuestPrice(main.id));
 
 	var dessert = model.getDish(dishList[2]);
 	this.dessert.html(dessert.name);
+	this.dessertPrice.html(model.getDishGuestPrice(dessert.id));
+
+	this.totalPrice.html(model.getTotalMenuPrice());
 
 
 
