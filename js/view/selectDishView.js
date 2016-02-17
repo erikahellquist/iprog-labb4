0355794a5308ecbad982a289dc8ad212e8881a0c
Erika Hellquist <erika.hellquist@live.com>
2016-02-17 12:18:49 +0100

    added boxes in sideview

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -4,12 +4,9 @@ var SelectDishView = function (container, model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 
-	//this.numberOfGuests = container.find("#numberOfGuests");
-	//this.numberOfGuests.html(model.getNumberOfGuests());
 
 	this.allDishes = container.find("#mainWindow");
 	
-
 
 	var dishList = "<br><br><br>"
 	var list = model.getMainDishes();
@@ -26,33 +23,7 @@ var SelectDishView = function (container, model) {
 		dishList = dishList + "<br></div>"
 	}
 
-//	dishList = dishList + "</div>"
-
 	this.allDishes.html(dishList);                                        
 
-
-
-	/*var dishList = "<table><tr>"
-	var list = model.getMainDishes();
-
-
-	for (i = 0; i < list.length; i++) {
-		dish = list[i]
-		if (i % 5 == 0 ) {
-			dishList = dishList + "</tr><tr>"
-		}
-			dishList = dishList + "<td><img src='images/" + dish.image + "'></img><br>"
-			dishList = dishList + dish.name
-			dishList = dishList + "<br>"
-			dishList = dishList + dish.description
-			dishList = dishList + "<br></td>"	
-	}
-
-	dishList = dishList + "</tr></table>"
-
-	this.allDishes.html(dishList);
-	
-	//this.allDishes.html(model.getDish(model.getSelectedDish(starter)));
-*/
 }
  
--- js/view/sideWindowView.js
+++ js/view/sideWindowView.js
@@ -24,8 +24,42 @@ var SideWindowView = function (container, model) {
 	this.numberOfGuests.html(model.getNumberOfGuests());
 
 
-
 	this.menuContainer = container.find("#menu");
+
+	var string = "<br>";
+
+	var menu = model.getFullMenu()
+
+	for (no in menu) {
+		var dish = model.getDish(menu[no]);
+
+		if (dish.id == model.getSelectedDishId()) {
+			string += "<div class='row' id='current'>"
+		}
+		else {
+			string += "<div class='row' id='noncurrent'>"
+		}
+		string += "<span class='left'>" + model.getNumberOfGuests() + "</span>"
+		string += dish.name
+		string += "<span class='right price'>"+ model.getDishGuestPrice(dish.id) + "</span>"
+		string += "</div>"
+	}
+
+	string += "<hr><span class='right price'>SEK " + model.getTotalMenuPrice() + "</span><br><br>"
+
+	string += "<a href='dinnerOverview.html'><button type='button' class='btn-default btn-block'>Confirm dinner</button></a><br>"
+
+	
+
+	this.menuContainer.html(string);
+
+
+
+
+
+
+
+	/* Kod som funkar
 
 	var string = "<br>";
 
@@ -47,6 +81,8 @@ var SideWindowView = function (container, model) {
 
 	this.menuContainer.html(string);
 
+
+	*/
 
 
 	
