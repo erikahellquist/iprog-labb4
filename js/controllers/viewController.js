3e6f7a6ce6ec3cdf16667f32f4ee2113e996ecdb
Erika Hellquist <erika.hellquist@live.com>
2016-02-19 14:48:58 +0100

    controllers

--- js/controllers/viewController.js
+++ js/controllers/viewController.js
@@ -1,7 +1,13 @@
 //viewController Object constructor
 var viewController = function(model) {
- 
 
+	$("#homePage").show();
+	$("#header").hide();
+	$("#sideWindowView").hide();
+	$("#recipeView").hide();
+	$("#selectDishView").hide();
+	$("#dinnerView").hide();
+	$("#dinnerPrepView").hide();
 	
 
 
