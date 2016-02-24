5c74cafb4e4ee06ff3e35619a205ec77f8a4cfb6
Erika Hellquist <erika.hellquist@live.com>
2016-02-24 22:39:34 +0100

    work on pending

--- js/controllers/recipeViewController.js
+++ js/controllers/recipeViewController.js
@@ -3,6 +3,7 @@ var RecipeViewController = function(view, model ,viewController) {
 	view.backButton.click(function(){
 		console.log("I backButton-funk");
 		viewController.showSelectDish();
+		model.setSelectedDishId(undefined);
 	});
 	
 	view.confirmButton.click(function(){
@@ -10,5 +11,6 @@ var RecipeViewController = function(view, model ,viewController) {
 		viewController.showSelectDish();
 		dishId = model.getSelectedDishId();
 		model.addDishToMenu(dishId);
+		model.setSelectedDishId(undefined);
 	});
 	}
